5611695e1734a3669351a5ac5a6a442d7de1ce66
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-06 07:28:11 +0700

    add firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCDlgspY0OodFuo-QnijbwDDqhO1i1k4ko",
+    authDomain: "react-firebase-crud-646fa.firebaseapp.com",
+    databaseURL: "https://react-firebase-crud-646fa.firebaseio.com",
+    projectId: "react-firebase-crud-646fa",
+    storageBucket: "react-firebase-crud-646fa.appspot.com",
+    messagingSenderId: "395514846645",
+    appId: "1:395514846645:web:21b18a4b825244ea280dfc",
+    measurementId: "G-DXHKZ7L12G"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
